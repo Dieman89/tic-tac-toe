4d97c0dea2c4cdc701d50874d263a07a9f0e7d35
Dieman <a.buonerba@hotmail.com>
2020-11-06 16:28:57 +0000

    Initial Framer Implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,56 @@
+import { motion } from "framer-motion";
 import React, { useState } from "react";
 import styled from "styled-components";
 import { Board, Winner } from "./Board";
 import { ResetScreen } from "./ResetScreen";
 import { StartScreen } from "./StartScreen";
 
-const BoardContainer = styled.div`
+const variants = {
+  game: {
+    opacity: 1,
+    scale: 1,
+    width: "500px",
+    height: "500px",
+    transition: {
+      type: "spring",
+      duration: 0.8,
+    },
+  },
+  start: {
+    opacity: 1,
+    scale: 1,
+    width: "200px",
+    height: "100px",
+    transition: {
+      type: "spring",
+      duration: 0.8,
+    },
+  },
+  reset: {
+    opacity: 1,
+    scale: 1,
+    width: "350px",
+    height: "300px",
+    transition: {
+      type: "spring",
+      duration: 0.8,
+    },
+  },
+
+  hidden: { opacity: 0, scale: 0.8 },
+};
+
+const BoardContainer = styled(motion.div).attrs(() => ({
+  initial: "hidden",
+  variants,
+}))`
   background: #ffffff;
-  width: 500px;
-  height: 500px;
   border-radius: 16px;
   box-shadow: -6px 10px 30px 4px #00000033;
   border: 20px solid #ffffff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 type GameState = "start" | "game" | "reset";
@@ -33,15 +73,18 @@ function App() {
     setGameState("game");
   };
   return (
-    <BoardContainer>
-      {
+    <>
+      <h1>Typescript Tic-Tac-Toe</h1>
+      <BoardContainer animate={gameState}>
         {
-          start: <StartScreen onStart={onStart} />,
-          game: <Board onGameEnd={onGameEnd} />,
-          reset: <ResetScreen winner={winner} onReset={onReset} />,
-        }[gameState]
-      }
-    </BoardContainer>
+          {
+            start: <StartScreen onStart={onStart} />,
+            game: <Board onGameEnd={onGameEnd} />,
+            reset: <ResetScreen winner={winner} onReset={onReset} />,
+          }[gameState]
+        }
+      </BoardContainer>
+    </>
   );
 }
 
